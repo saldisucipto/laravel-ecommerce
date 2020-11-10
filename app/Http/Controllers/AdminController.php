0d60163ae17658feec97a3cb43c0c8e13dfcd649
Saldi Sucipto <saldisucipto@gmail.com>
2020-11-10 22:35:26 +0700

    Protect Admin Route With Session

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,10 +13,11 @@ class AdminController extends Controller
         if($request->isMethod('POST')){
             $data = $request->all();
             if(Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'admin' => '1'])){
+              // Protection with session 
+              Session::put('adminSession', $data['email']);
               return  \redirect('/dashboard');
             }else{
-        return \redirect('/admin')->with('pesan_flash_error', 'Password Dan Email Anda Salah!');
-                
+               return \redirect('/admin')->with('pesan_flash_error', 'Password Dan Email Anda Salah!');   
             };
         }
         return \view('login');
@@ -25,6 +26,11 @@ class AdminController extends Controller
 
     // home function 
     public function home(){
+        if(Session::has('adminSession')){
+            // perform all dashboard task
+        }else{
+            return \redirect('/admin')->with('pesan_flash_error', 'Anda Harus Login Dulu!');   
+        }
         return view('backend.home.index');
     }
 
